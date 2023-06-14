97f54afbdf1a87f3edc1ebcc5883cc8d4a5d9ad9
Deaths-Door <aaravaditya51@gmail.com>
2023-06-14 13:17:24 +0200

    Declaration done

--- src/lib/declaration.rs
+++ src/lib/declaration.rs
@@ -3,3 +3,31 @@ pub struct Declaration<'a> {
     encoding: &'a str,
     standalone: Option<&'a str>,
 }
+
+impl Declaration<'_> {
+    pub fn new(version: &str, encoding: &str) -> Self {
+        Declaration {
+            version: version,
+            encoding: encoding,
+            standalone : None,
+        }
+    }
+
+    pub fn with_standlone(&self,standalone : &str) -> Self {
+        self.standalone = Some(standalone);
+        self
+    }
+}
+
+impl<'a> fmt::Display for Declaration<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, r#"<?xml version="{}" encoding="{}""#, self.version, self.encoding)?;
+        
+        if let Some(standalone) = self.standalone {
+            write!(f, r#" standalone="{}""#, standalone)?;
+        }
+        
+        write!(f, "?>")?;
+        Ok(())
+    }
+}
