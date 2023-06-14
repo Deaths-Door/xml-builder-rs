9506b61404112ad023f404a300021227f0e86e13
Deaths-Door <aaravaditya51@gmail.com>
2023-06-14 13:22:13 +0200

    Attribute done

--- src/lib/attribute.rs
+++ src/lib/attribute.rs
@@ -1,5 +1,32 @@
 pub struct Attribute<'a> {
     name: &'a str,
     value: &'a str,
-    namespace: Option<&'a str>,
+    namespace: Option<Namespace>,
+}
+
+impl Attribute {
+    pub fn new(name: &str, value: &str) -> Self {
+        Attribute {
+            name,
+            value,
+            None,
+        }
+    }
+    pub fn with_namespace(namespace: Namespace) -> Self {
+        self.namespace = Some(namespace);
+        self
+    }
+}
+
+impl fmt::Display for Attribute<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(namespace) = self.namespace {
+            write!(f, "{}:", namespace.prefix)?;
+        };
+
+        write!(f, "{}", self.name)?;
+        
+        write!(f, "=\"{}\"", self.value)?;
+        Ok(())
+    }
 }
